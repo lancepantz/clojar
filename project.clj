9d91821382fc465a08dcd5865cf8bf7bc7e5a21e
lance bradley <l@ncebradley.org>
2011-11-18 17:25:21 -0800

    put project in lancepantz group

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject clojar "1.0.0-SNAPSHOT"
+(defproject org.clojars.lancepantz/clojar "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.2.1"]])
